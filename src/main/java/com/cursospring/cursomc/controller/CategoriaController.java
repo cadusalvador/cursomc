930bafa4fc1e7d4c46ea308ea5e92cf950a4c556
Carlos Eduardo Salvador <carlos1992.ces@gmail.com>
2023-07-01 22:07:54 -0300

    Testando primeiro domain

--- src/main/java/com/cursospring/cursomc/controller/CategoriaController.java
+++ src/main/java/com/cursospring/cursomc/controller/CategoriaController.java
@@ -1,15 +1,27 @@
 package com.cursospring.cursomc.controller;
 
+import com.cursospring.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar() {
-        return "REST esta funcionando";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
